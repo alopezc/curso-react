37510bdb7f83cabf0c9c2e9eb4594a3d5076ebf1
Aaron Fernando Lopez <neochronos@gmail.com>
2019-01-30 15:17:40 -0600

    se inicializa proyecto stream

---
+++ strams/client/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					App header
+				</header>
+			</div>
+		);
+	}
+}
+
+export default App;
